db75e41a890273db24f4e46f7db9405aa5beac42
Van064886 <atadev13@gmail.com>
2023-11-12 23:24:51 +0100

    feat: add subscription request added

--- app/Http/Controllers/API/AdsSubscriptionController.php
+++ app/Http/Controllers/API/AdsSubscriptionController.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Core\AdsSharingHistories\Repositories\Interfaces\AdsSharingHistoryRepositoryInterface;
+use App\Core\AdsSubscriptions\Repositories\Interfaces\AdsSubscriptionRepositoryInterface;
+use App\Http\Controllers\ApiBaseController;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class AdsSubscriptionController extends Controller
+class AdsSubscriptionController extends ApiBaseController
 {
-    //
+    /**
+     * Class constructor
+     */
+    public function __construct(
+        private AdsSubscriptionRepositoryInterface $adsSubscriptionRepo,
+        private AdsSharingHistoryRepositoryInterface  $adsSharingHistoryRepo
+    ) {
+    }
+
+    public function addSubscription(AddSubscriptionRequest $request)
 }
