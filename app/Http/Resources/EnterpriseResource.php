abdb373f064fe637fe3afffbbab04f1afbdce6c4
Van064886 <atadev13@gmail.com>
2023-11-13 21:29:53 +0100

    fix: create enterprise error

--- app/Http/Resources/EnterpriseResource.php
+++ app/Http/Resources/EnterpriseResource.php
@@ -14,9 +14,6 @@ class EnterpriseResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $this->load('enterprise', 'enterprise.owner', 'enterprise.activity_sector',
-            'actionType');
-
         return [
             'id' => $this->id,
             'name' => $this->name,
